4a9a3976d719ae9fb6561ee6c7c64a97a3b140d1
Célian Frasca <celian.frasca@gmail.com>
2023-11-24 11:48:50 +0100

    ajout du docker

--- src/main/java/com/webservice/consumer/listener/UserRegistrationListener.java
+++ src/main/java/com/webservice/consumer/listener/UserRegistrationListener.java
@@ -1,2 +1,35 @@
-package com.webservice.consumer.listener;public class UserRegistrationListener {
+package com.webservice.consumer.listener;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webservice.consumer.User.User;
+import com.webservice.consumer.service.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserRegistrationListener {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @RabbitListener(queues = "rabbitmq")
+    public void receiveMessage(String message) {
+        emailService.sendVerificationEmail(Objects.requireNonNull(extractUserFromMessage(message)));
+    }
+
+    private User extractUserFromMessage(String message) {
+        try {
+            return objectMapper.readValue(message, User.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
