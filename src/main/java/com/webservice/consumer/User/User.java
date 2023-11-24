439c1e92a57363120aa74f1d108d28ee5b122c58
Célian Frasca <celian.frasca@gmail.com>
2023-11-24 11:48:50 +0100

    ajout du docker

--- src/main/java/com/webservice/consumer/User/User.java
+++ src/main/java/com/webservice/consumer/User/User.java
@@ -1,2 +1,14 @@
-package com.webservice.consumer.User;public class User {
+package com.webservice.consumer.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class User implements Serializable {
+    private String name;
+    private String email;
+    private String verificationToken;
 }
